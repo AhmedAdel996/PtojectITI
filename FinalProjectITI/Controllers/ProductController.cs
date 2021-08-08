caef80688fd6e05bb11e360b53189a929a4cdb98
Ahmed <engahmedadel1996@gmail.com>
2021-08-08 03:44:22 +0200

    initial commit

---
+++ FinalProjectITI/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProjectITI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowCategoris()
+        {
+            return PartialView("_ViewCategories");
+        }
+
+
+        public IActionResult ShowProducts()
+        {
+            return PartialView("_ViewProducts");
+        }
+
+    }
+}
